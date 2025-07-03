a8a92983ba99278094990604e00eeb9d07cb1ef3
Ljavuras <ljavuras.py@gmail.com>
2025-07-03 08:17:03 +0800

    feat: 3 new strategies

--- src/main.py
+++ src/main.py
@@ -6,7 +6,11 @@ from sklearn.base import clone
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import LeaveOneOut, cross_val_score
+from sklearn.metrics import accuracy_score, make_scorer
+import warnings
+
+warnings.filterwarnings('ignore') 
 
 def printLeaveOneOutResult(model, X, y):
     loo = LeaveOneOut()
@@ -47,12 +51,24 @@ def printLeaveOneOutResult(model, X, y):
     print(f"Correct: {correct}")
     print(recommendations)
 
-def embed_posting(postings, embedder):
+def embed_item_only(postings, embedder):
     return embedder.encode(postings['商品品項'].to_list())
 
-transform_strategies = {
-    'item_only': FunctionTransformer(embed_posting)
-}
+def embed_company_item(postings, embedder):
+    return embedder.encode((postings['公司名稱'] + postings['商品品項']).to_list())
+
+def embed_company_item_sep(postings, embedder):
+    company_embeddings = embedder.encode(postings['公司名稱'].to_list())
+    item_embeddings = embedder.encode(postings['商品品項'].to_list())
+    return np.array([company_embeddings[i] + item_embeddings[i] for i in range(len(postings))])
+
+def embed_company_scope_item(postings, embedder):
+    return embedder.encode(
+        postings[['公司名稱', '行業1', '行業2', '行業3', '行業4', '商品品項']]
+        .fillna('')
+        .apply(lambda x: ''.join(x), axis=1)
+        .to_list()
+    )
 
 if __name__ == "__main__":
     postings = testpostings.postings()
@@ -62,10 +78,28 @@ if __name__ == "__main__":
     # y = postings['nelly']
 
     embedder = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
+    # pipe = Pipeline([
+    #     ('transformer', transform_strategies['item_only'].set_params(kw_args={'embedder': embedder})),
+    #     ('classifier', KNeighborsClassifier(weights='distance'))
+    # ])
 
-    pipe = Pipeline([
-        ('transformer', transform_strategies['item_only'].set_params(kw_args={'embedder': embedder})),
-        ('classifier', KNeighborsClassifier(weights='distance'))
-    ])
+    # printLeaveOneOutResult(pipe, X, y)
 
-    printLeaveOneOutResult(pipe, X, y)
+    transform_strategies = {
+        # 'item_only': FunctionTransformer(embed_item_only),  # 17
+        'company_item': FunctionTransformer(embed_company_item),  # 54
+        # 'company_scope_item': FunctionTransformer(embed_company_scope_item),  # 60, significantly longer time
+        'company_item_sep': FunctionTransformer(embed_company_item_sep),  #47
+    }
+
+    accuracies = {}
+    for strategy, transformer in transform_strategies.items():
+        pipe = Pipeline([
+            ('transformer', transformer.set_params(kw_args={'embedder': embedder})),
+            ('classifier', KNeighborsClassifier(weights='distance'))
+        ])
+
+        scores = cross_val_score(pipe, X, y, cv=LeaveOneOut(), scoring=make_scorer(accuracy_score))
+        accuracies[strategy] = sum(scores)
+
+    print(accuracies)
