696d6457cb29e9f07077a1f275034c40090a8013
Ljavuras <ljavuras.py@gmail.com>
2025-07-20 01:12:30 +0800

    feat: establish screen manager for screen stacks

--- src/app/Catcountants/main.py
+++ src/app/Catcountants/main.py
@@ -1,11 +1,21 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+
+Builder.load_file("main.kv")
+
+class BaseScreen(MDScreen):
+    pass
+
+class StackScreenManager(MDScreenManager):
+    pass
 
 class CatcountantsApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
 
-        return Builder.load_file("main.kv")
-    
+        return StackScreenManager()
+
 if __name__ == "__main__":
     CatcountantsApp().run()
