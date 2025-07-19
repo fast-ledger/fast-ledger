7107f03decd069595d7b087b5464b04a900a2912
Ljavuras <ljavuras.py@gmail.com>
2025-07-20 05:14:20 +0800

    fix: use resolution instead of viewport size

--- src/app/Catcountants/devices/samsung_a32.py
+++ src/app/Catcountants/devices/samsung_a32.py
@@ -4,8 +4,8 @@ from os import environ
 
 density = 2.625
 dpi = 411
-width, height = 412, 915
-scale = .48
+width, height = 1080, 2400
+scale = .2955
 environ['KIVY_METRICS_FONTSCALE'] = '1'
 environ['KIVY_METRICS_DENSITY'] = str(density * scale)
 environ['KIVY_DPI'] = str(dpi * scale)
