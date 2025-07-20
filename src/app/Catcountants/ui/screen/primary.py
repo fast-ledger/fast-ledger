cfa27398d2e6954a2c480f0b12325b2ba45eecb0
Ljavuras <ljavuras.py@gmail.com>
2025-07-20 18:32:13 +0800

    feat: change title when switching screen

--- src/app/Catcountants/ui/screen/primary.py
+++ src/app/Catcountants/ui/screen/primary.py
@@ -1,10 +1,14 @@
+from kivy.properties import StringProperty
 from kivymd.uix.screen import MDScreen
 from utilities.loadkv import loadkv
 
 loadkv(__file__)
 
 class PrimaryScreen(MDScreen):
+    title = StringProperty()
+
     def on_switch_tabs(self, bar, item, item_icon, item_text):
+        """Change screen and title when switching tabs by bottom nav bar"""
         screen_manager = self.ids.primary_screen_manager
         current_index = screen_manager.screen_names.index(screen_manager.current)
         next_index = screen_manager.screen_names.index(item.get_screen_name())
@@ -14,3 +18,5 @@ class PrimaryScreen(MDScreen):
         else:
             screen_manager.transition.direction = 'right'
         screen_manager.current = item.get_screen_name()
+
+        self.title = self.ids.primary_screen_manager.current_screen.title
